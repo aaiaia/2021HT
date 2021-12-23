8e328a3b74e8c81c1c25656ae86673cf387803ef
PleYa <aaiaia.gg@gmail.com>
2021-12-23 10:47:55 +0900

    add main

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void loadFile(char* path)
+{
+	FILE* fp = NULL;
+	fp = fopen((const char*)path, "r");
+
+}
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
